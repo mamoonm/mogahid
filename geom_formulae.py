40938483a9c42d9bf2611880a8e180175a261eb9
mamoon <mogaid@aims.edu.gh>
2014-08-30 10:44:18 +0000

    test1

--- geom_formulae.py
+++ geom_formulae.py
@@ -13,8 +13,37 @@ def circle_area(radius: Number) ->Number:
     return pi*radius**2
 print(circle_area(7))
 
+def triangle_area(base, height):
+    """
+    calculate are of a triangle
+    :param base: base of a triangle
+    :param heigh: heigh of a triangle
+    :return:area of atringle
+    >>> triangle_area(4, 5)
+    """
+    return 0.5*base*height
 
+print(triangle_area(2,4))
 
+def rectangle_area(length,width):
+    """
+    calculates the area of rectangle
+    :param length: length of a rectangle
+    :param width: width of a rectangle
+    :return:area of a rectangle
+    >>> rectangle_area(4, 2)
+    8
+    """
+    return length*width
+print(rectangle_area(4, 2))
 
-
-
+def sphere_volume(radius):
+    """
+    calculate the area of sphere
+    :param radius: radius of sphere
+    :return:the volume of the sphere(in cubic units of the radius)
+    >>> sphere _volume
+        1436.7550402417319
+    """
+    return 4*pi*radius*radius*radius/3
+print(sphere_volume(7))
